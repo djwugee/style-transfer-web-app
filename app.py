5d3538edc0533f37dcc2cac0859f27583d989deb
Kaaneii Williams <djwugee@gmail.com>
2023-07-03 18:17:16 +1200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 import streamlit as st
-
 from data import *
 from input import image_input, webcam_input
 
 st.title("Neural Style Transfer")
 st.sidebar.title('Navigation')
-method = st.sidebar.radio('Go To ->', options=['Webcam', 'Image'])
-st.sidebar.header('Options')
 
+method = st.sidebar.radio('Go To ->', options=['Webcam', 'Image', 'Video']) # Add 'Video' option
+
+st.sidebar.header('Options')
 style_model_name = st.sidebar.selectbox("Choose the style model: ", style_models_name)
 
 if method == 'Image':
     image_input(style_model_name)
-else:
+elif method == 'Webcam':
     webcam_input(style_model_name)
+else: # Video input functionality
+    st.header("Video Input")
+    video_file = st.sidebar.file_uploader("Choose a Video File", type=["mp4", "webm"])
+    if video_file is not None:
+        st.video(video_file)
+    else:
+        st.warning("Upload a Video File")
