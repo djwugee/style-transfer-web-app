86fe09f66be158d39d6dab1de9b5b3fe8a8cf05b
Kaaneii Williams <djwugee@gmail.com>
2023-07-19 20:10:22 +1200

    Update fix app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from data import *
-from input import image_input, webcam_input
+from input import image_input, webcam_input ,video_input
 
 st.title("Neural Style Transfer")
 st.sidebar.title('Navigation')
@@ -12,12 +12,7 @@ style_model_name = st.sidebar.selectbox("Choose the style model: ", style_models
 
 if method == 'Image':
     image_input(style_model_name)
-elif method == 'Webcam':
+else method == 'Webcam':
     webcam_input(style_model_name)
-else: # Video input functionality
-    st.header("Video Input")
-    video_file = st.sidebar.file_uploader("Choose a Video File", type=["mp4", "webm"])
-    if video_file is not None:
-        st.video(video_file)
-    else:
-        st.warning("Upload a Video File")
+else: method == 'Video':
+    video_input(style_model_name)
