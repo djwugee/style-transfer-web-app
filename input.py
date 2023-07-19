69023881952a4aea50d15cccb1a816856de56e52
Kaaneii Williams <djwugee@gmail.com>
2023-07-19 20:09:08 +1200

    Update fix

--- input.py
+++ input.py
@@ -1,95 +1,169 @@
 import threading
-import cv2
 import numpy as np
-from PIL import Image
 import streamlit as st
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, ClientSettings
+from PIL import Image
+import cv2
+import imutils
+from neural_style_transfer import get_model_from_path, style_transfer
+from data import *
 
-# Define the VideoTransformerBase class
-class VideoTransformerBase:
-    def transform(self, frame):
-        # Implement your video transformation logic here
-        pass
-
-WIDTH = st.sidebar.select_slider('QUALITY (May reduce the speed)', list(range(150, 501, 50)))
-
-class NeuralStyleTransferTransformer(VideoTransformerBase):
-    _width = WIDTH
-    _model_name = style_model_name
-    _model = None
-
-    def __init__(self) -> None:
-        self._model_lock = threading.Lock()
-        self._width = WIDTH
-        self._update_model()
-
-    def set_width(self, width):
-        update_needed = self._width != width
-        self._width = width
-        if update_needed:
-            self._update_model()
-
-    def update_model_name(self, model_name):
-        update_needed = self._model_name != model_name
-        self._model_name = model_name
-        if update_needed:
-            self._update_model()
-
-    def _update_model(self):
-        style_model_path = style_models_dict.get(self._model_name)
-        if style_model_path is None:
-            raise ValueError(f"Invalid style model name: {self._model_name}")
-        with self._model_lock:
-            self._model = get_model_from_path(style_model_path)
-
-    def transform(self, frame):
-        image = frame.to_ndarray(format="bgr24")
-        if self._model is None:
-            return image
-        orig_h, orig_w = image.shape[0:2]
-        input = np.asarray(Image.fromarray(image).resize((self._width, int(self._width * orig_h / orig_w))))
-        with self._model_lock:
-            transferred = style_transfer(input, self._model)
-        result = Image.fromarray((transferred * 255).astype(np.uint8))
-        return np.asarray(result.resize((orig_w, orig_h)))
-
-style_model_name = ""
-
-# Define your style model name
-style_models_dict = {}
-
-# Define your content images name
-content_images_name = []
-
-# Define your content images dictionary
-content_images_dict = {}
-
-video_file = st.sidebar.file_uploader("Choose a Video File", type=["mp4", "webm"])
-if video_file is not None:
-    st.video(video_file)
-    style_model_path = style_models_dict.get(style_model_name)
-    if style_model_path is None:
-        raise ValueError(f"Invalid style model name: {style_model_name}")
+def image_input(style_model_name):
+    style_model_path = style_models_dict[style_model_name]
     model = get_model_from_path(style_model_path)
-
-    # Define the get_model_from_path function
 
     if st.sidebar.checkbox('Upload'):
         content_file = st.sidebar.file_uploader("Choose a Content Image", type=["png", "jpg", "jpeg"])
     else:
         content_name = st.sidebar.selectbox("Choose the content images:", content_images_name)
-        content_file = content_images_dict.get(content_name)
+        content_file = content_images_dict[content_name]
 
     if content_file is not None:
         content = Image.open(content_file)
-        content = np.array(content)
+        content = np.array(content) #pil to cv
         content = cv2.cvtColor(content, cv2.COLOR_RGB2BGR)
     else:
         st.warning("Upload an Image OR Untick the Upload Button")
         st.stop()
 
-    content = imutils.resize(content, width=WIDTH)  # Make sure to import imutils
-    generated = style_transfer(content, model)  # Define the style_transfer function
+    WIDTH = st.sidebar.select_slider('QUALITY (May reduce the speed)', list(range(150, 501, 50)), value=200)
+    content = imutils.resize(content, width=WIDTH)
+    generated = style_transfer(content, model)
     st.sidebar.image(content, width=300, channels='BGR')
     st.image(generated, channels='BGR', clamp=True)
+
+def video_input(style_model_name):
+    style_model_path = style_models_dict[style_model_name]
+    model = get_model_from_path(style_model_path)
+
+    if st.sidebar.checkbox('Upload'):
+        video_file = st.sidebar.file_uploader("Choose a Video File", type=["mp4", "webm"])
+    else:
+        video_name = st.sidebar.selectbox("Choose the video:", video_files_name)
+        video_file = video_files_dict[video_name]
+
+    if video_file is not None:
+        video = cv2.VideoCapture(video_file)
+        fps = video.get(cv2.CAP_PROP_FPS)
+    else:
+        st.warning("Upload a Video OR Untick the Upload Button")
+        st.stop()
+
+    WIDTH = st.sidebar.select_slider('QUALITY (May reduce the speed)', list(range(150, 501, 50)), value=200)
+
+    class NeuralStyleTransferVideoTransformer(VideoTransformerBase):
+        _width = WIDTH
+        _model_name = style_model_name
+        _model = None
+
+        def __init__(self) -> None:
+            self._model_lock = threading.Lock()
+            self._width = WIDTH
+            self._update_model()
+
+        def set_width(self, width):
+            update_needed = self._width != width
+            self._width = width
+            if update_needed:
+                self._update_model()
+
+        def update_model_name(self, model_name):
+            update_needed = self._model_name != model_name
+            self._model_name = model_name
+            if update_needed:
+                self._update_model()
+
+        def _update_model(self):
+            style_model_path = style_models_dict[self._model_name]
+            with self._model_lock:
+                self._model = get_model_from_path(style_model_path)
+
+        def transform(self, frame):
+            image = frame.to_ndarray(format="bgr24")
+            if self._model is None:
+                return image
+            orig_h, orig_w = image.shape[0:2]
+            # cv2.resize used in a forked thread may cause memory leaks
+            input = np.asarray(Image.fromarray(image).resize((self._width, int(self._width * orig_h / orig_w))))
+            with self._model_lock:
+                transferred = style_transfer(input, self._model)
+            result = Image.fromarray((transferred * 255).astype(np.uint8))
+            return np.asarray(result.resize((orig_w, orig_h)))
+
+    ctx = webrtc_streamer(
+        client_settings=ClientSettings(
+            rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+            media_stream_constraints={"video": True, "audio": False},
+        ),
+        video_transformer_factory=NeuralStyleTransferVideoTransformer,
+        key="neural-style-transfer",
+    )
+
+    if ctx.video_transformer:
+        ctx.video_transformer.set_width(WIDTH)
+        ctx.video_transformer.update_model_name(style_model_name)
+
+def webcam_input(style_model_name):
+    st.header("Webcam Live Feed")
+    WIDTH = st.sidebar.select_slider('QUALITY (May reduce the speed)', list(range(150, 501, 50)))
+
+    class NeuralStyleTransferWebcamTransformer(VideoTransformerBase):
+        _width = WIDTH
+        _model_name = style_model_name
+        _model = None
+
+        def __init__(self) -> None:
+            self._model_lock = threading.Lock()
+            self._width = WIDTH
+            self._update_model()
+
+        def set_width(self, width):
+            update_needed = self._width != width
+            self._width = width
+            if update_needed:
+                self._update_model()
+
+        def update_model_name(self, model_name):
+            update_needed = self._model_name != model_name
+            self._model_name = model_name
+            if update_needed:
+                self._update_model()
+
+        def _update_model(self):
+            style_model_path = style_models_dict[self._model_name]
+            with self._model_lock:
+                self._model = get_model_from_path(style_model_path)
+
+        def transform(self, frame):
+            image = frame.to_ndarray(format="bgr24")
+            if self._model is None:
+                return image
+            orig_h, orig_w = image.shape[0:2]
+            input = np.asarray(Image.fromarray(image).resize((self._width, int(self._width * orig_h / orig_w))))
+            with self._model_lock:
+                transferred = style_transfer(input, self._model)
+            result = Image.fromarray((transferred * 255).astype(np.uint8))
+            return np.asarray(result.resize((orig_w, orig_h)))
+
+    ctx = webrtc_streamer(
+        client_settings=ClientSettings(
+            rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+            media_stream_constraints={"video": True, "audio": False},
+        ),
+        video_transformer_factory=NeuralStyleTransferWebcamTransformer,
+        key="neural-style-transfer",
+    )
+
+    if ctx.video_transformer:
+        ctx.video_transformer.set_width(WIDTH)
+        ctx.video_transformer.update_model_name(style_model_name)
+
+style_model_name = st.sidebar.selectbox("Choose the style model:", style_models_name)
+input_type = st.sidebar.radio("Select Input Type:", ("Image", "Video", "Webcam"))
+
+if input_type == "Image":
+    image_input(style_model_name)
+elif input_type == "Video":
+    video_input(style_model_name)
 else:
-    st.warning("No video file selected.")
+    webcam_input(style_model_name)
