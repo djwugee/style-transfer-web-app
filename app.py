aa9d53cc2d7f191884c8e03af23b0620e5728aba
Kaaneii Williams <djwugee@gmail.com>
2024-08-31 01:56:56 +1200

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import streamlit as st
+
 from data import *
-from input import image_input, webcam_input ,video_input
+from input import image_input, webcam_input
 
 st.title("Neural Style Transfer")
 st.sidebar.title('Navigation')
-
-method = st.sidebar.radio('Go To ->', options=['Webcam', 'Image', 'Video']) # Add 'Video' option
-
+method = st.sidebar.radio('Go To ->', options=['Webcam', 'Image'])
 st.sidebar.header('Options')
+
 style_model_name = st.sidebar.selectbox("Choose the style model: ", style_models_name)
 
 if method == 'Image':
     image_input(style_model_name)
-elif method == 'Webcam':
+else:
     webcam_input(style_model_name)
-elif method == 'Video':
-    video_input(style_model_name)
